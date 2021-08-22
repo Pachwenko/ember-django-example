09e9c6353583d246e697c09f115784520865688f
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2021-08-22 13:57:27 -0500

    add mapbox thumbnails to rentals

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -47,5 +47,8 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN =
+    'pk.eyJ1IjoicGFjaHdlbmtvIiwiYSI6ImNrc25razBjbzNmNWMyem5xb2x4dnk4dmYifQ.SxfIbHD7rRLiJ4X6pFiqAA';
+
   return ENV;
 };
