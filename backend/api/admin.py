ba3e6f8e5706a7dd3364f1ac8a7f15d149430dc1
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2021-09-22 21:47:16 -0500

    add django admin pages

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models.rental import Rental
+from api.models.location import Location
+
+
+admin.site.register(Rental)
+admin.site.register(Location)
