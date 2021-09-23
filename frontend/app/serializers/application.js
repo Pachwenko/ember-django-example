6f76b20396cf6c3ca9c37bc930bec12ab5488679
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2021-09-22 22:10:08 -0500

    fix styling in frontend

--- frontend/app/serializers/application.js
+++ frontend/app/serializers/application.js
@@ -1,5 +1,5 @@
-import DS from 'ember-data';
+import JSONAPISerializer from '@ember-data/serializer/json-api';
 
 // using JSONAPI for everything makes life much easier on the frontend
-// but it can look like magic
-export default DS.JSONAPISerializer;
+// but it can look like magic as we don't make any API calls directly
+export default JSONAPISerializer;
