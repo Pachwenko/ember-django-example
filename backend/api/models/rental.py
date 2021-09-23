f3ce3bc97f4c2514da5bc7d8ffa3dbfa2320ec04
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2021-09-22 23:39:36 -0500

    change category to be choice field to match frontend

--- backend/api/models/rental.py
+++ backend/api/models/rental.py
@@ -5,11 +5,18 @@ from api.models.location import Location
 
 
 class Rental(models.Model):
+    STANDALONE = 'Standalone'
+    COMMUNITY = 'Community'
+    CATEGORY_CHOICES = [
+        (STANDALONE, 'Standalone'),
+        (COMMUNITY, 'Community')
+    ]
+
     owner = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, choices=CATEGORY_CHOICES)
     description = models.TextField()
     image = models.TextField()
     bedrooms = models.IntegerField(default=1)
--- backend/api/tests/factories/rental.py
+++ backend/api/tests/factories/rental.py
@@ -1,4 +1,5 @@
 import factory  # https://github.com/FactoryBoy/factory_boy
+import factory.fuzzy
 from api.models.rental import Rental
 
 from api.tests.factories.location import LocationFactory
@@ -12,6 +13,7 @@ class RentalFactory(factory.django.DjangoModelFactory):
     type = factory.Faker('word')
     city = factory.Faker('word')
     title = factory.Faker('word')
+    category = factory.fuzzy.FuzzyChoice(['Standalone', 'Community'])
     category = factory.Faker('word')
     description = factory.Faker('paragraph')
     image = factory.Faker('image_url', width=600, height=600)
