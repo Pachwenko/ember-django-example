b449e6d95e18ef3b776b069735082d17c26ed72f
pachwenko <pmccart4096@gmail.com>
2021-10-12 19:05:26 -0500

    flake8 fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,7 +25,7 @@ from api.views.rental import RentalViewSet
 api_doc_urls = [
     path('swagger-ui/', TemplateView.as_view(
         template_name='swagger-ui.html',
-        extra_context={'schema_url':'openapi-schema'}
+        extra_context={'schema_url': 'openapi-schema'}
     ), name='swagger-ui'),
 ]
 
